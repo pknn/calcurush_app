38c695d2c42d783621dae0194fce6a63e782bdef
Pakanon Pantisawat <pakanon.pk@gmail.com>
2019-05-02 21:03:44 +0700

    Add question and timer

--- src/views/Level.vue
+++ src/views/Level.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="page-wrapper">
+    <div class="countdown-timer"></div>
     <div class="pause-screen"></div>
     <div class="overlay"></div>
     <div class="gameplay">
@@ -7,7 +8,7 @@
         <div class="side pause-button">
           <font-awesome-icons :icon="['fas', 'pause']"></font-awesome-icons>
         </div>
-        <div class="timer flex-1"><timer /></div>
+        <div class="timer flex-1"><timer ref="timer" /></div>
         <div class="side score">
           <div class="header">
             <span>Score</span>
@@ -17,7 +18,13 @@
           </div>
         </div>
       </div>
-      <div class="body"></div>
+      <div class="body">
+        <question
+          ref="question"
+          @onCorrect="correct"
+          @onIncorrect="incorrect"
+        />
+      </div>
     </div>
   </div>
 </template>
@@ -25,8 +32,10 @@
 <style lang="sass" scoped>
 .page-wrapper
   @apply flex flex-col justify-center items-center
+  .overlay
+    @apply absolute pin h-full w-full bg-black opacity-75 z-40
   .gameplay
-    @apply w-full
+    @apply w-full h-full
     .head-content
       @apply flex w-full items-center justify-center
       .side
@@ -42,19 +51,30 @@
 
 <script>
 import Timer from '@/components/Timer'
+import Question from '@/components/Question'
 
 export default {
-  components: { Timer },
+  components: { Timer, Question },
   data() {
     return {
       score: 0,
       state: 'stopped'
     }
   },
+  mounted() {},
   methods: {
     togglePause() {},
     levelSelect() {},
-    mainMenu() {}
+    mainMenu() {},
+    correct() {
+      this.score++
+      this.$refs.question.init()
+      this.$refs.timer.add(2)
+    },
+    incorrect() {
+      this.$refs.question.init()
+      this.$refs.timer.deduct(3)
+    }
   }
 }
 </script>
