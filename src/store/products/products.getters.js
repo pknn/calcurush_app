40f6885549ccf8d84905922a0ec68fc7a9104ec1
Pakanon Pantisawat <pakanon.pk@gmail.com>
2019-05-02 19:47:55 +0700

    Edit config

--- src/store/products/products.getters.js
+++
@@ -1,7 +0,0 @@
-export default {
-  /**
-   * Check if a product has deletion pending
-   */
-  isProductDeletionPending: state => productId =>
-    state.productDeletionPending.includes(productId)
-}
